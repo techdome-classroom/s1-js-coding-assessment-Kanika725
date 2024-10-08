57a569e8958bb94651ad4e38eb31803b5c732615
Kanika Sahu <142916615+Kanika725@users.noreply.github.com>
2024-10-08 12:31:55 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,19 +2,14 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
-   // Edge case: if the grid is empty
    if (!grid || grid.length === 0) return 0;
 
    let totalIslands = 0;
    const rows = grid.length;
    const cols = grid[0].length;
- 
-   // Helper function for Depth First Search
    const dfs = (i, j) => {
-     // If out of bounds or the current cell is water, return
      if (i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j] === 'W') return;
  
-     // Mark the current cell as visited by changing it to water
      grid[i][j] = 'W';
  
      // Explore in all four directions
